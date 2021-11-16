542ba50db4de2b1f9975ee901239135a79121739
HugoMarrugo <hugoandresmarrugopolo@gmail.com>
2021-11-15 20:11:09 -0500

    Microservicio Funcional, sin Choices

---
+++ apps/users/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,BaseUserManager
+from django.contrib.auth.hashers import make_password
+
+
+class UserManager(BaseUserManager, models.Manager):
+#creacion de usuarios, recibe varios parametros, de acuerdo a lo que esta en nuesto modelo users
+    def _create_user(self,cedula, username, email,nombres,apellidos, password,
+     is_staff, is_superuser, **extra_fields):
+
+        if not username:
+            raise ValueError('Users must have an username')
+
+
+        user = self.model(
+            cedula=cedula,
+            username=username,
+            email = self.normalize_email(email),
+            nombres=nombres,
+            apellidos=apellidos,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self,cedula, username, email,nombres,apellidos, password=None, **extra_fields):
+        return self._create_user(cedula,username, email,nombres,apellidos,password,False, False, **extra_fields)
+
+    def create_superuser(self,cedula, username, email,nombres,apellidos, password=None, **extra_fields):
+        return self._create_user(cedula,username, email,nombres,apellidos, password, True, True, **extra_fields)
+    
+
+class Usuario(AbstractBaseUser,PermissionsMixin):
+    cedula = models.CharField(max_length=50,primary_key=True)
+    username = models.CharField('Nombre de Usuario', max_length=50,unique=True)
+    email = models.EmailField('Correo Electronico', max_length=254,unique=True)
+    telefono_celular = models.CharField('Numero de Telefono Celular', max_length=10,blank=True,null=True)
+    nombres = models.CharField('Nombres', max_length=50)
+    apellidos = models.CharField('Apellidos', max_length=50)
+
+    is_active = models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+
+    def save(self, **kwargs):
+        some_salt = 'mMUj0DrIK6vgtdIYepkIxN' 
+        self.password = make_password(self.password, some_salt)
+        super().save(**kwargs)
+
+
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
+    
+    objects = UserManager()
+
+
+    #Campo que usara como identificador del usuario
+    USERNAME_FIELD = 'cedula'
+
+    #Campos que requerira para crear el usuario
+    REQUIRED_FIELDS = ['username','email','nombres','apellidos']
+
+
+    def __str__(self):
+        return f'{self.nombres} {self.apellidos}'
+
+    def has_perm(self,perm,obj  = None):
+        return True
+
+    def has_module_perms(self,app_label):
+        return True
+
+
+
+# Create your models here.
---
+++ apps/users/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from rest_framework import status, views
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from apps.users.serializers import UserSerializer
+
+from rest_framework import generics
+from apps.users.models import Usuario
+
+#Imports para verify Token
+
+from django.conf import settings
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenVerifyView
+from rest_framework_simplejwt.backends import TokenBackend
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.serializers import TokenVerifySerializer
+
+
+
+
+
+# Create your views here.
+
+from rest_framework import status, views
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from apps.users.serializers import UserSerializer
+
+
+class UserCreateView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        
+        tokenData = {"cedula":request.data["cedula"],
+                    "password":request.data["password"]}
+        tokenSerializer = TokenObtainPairSerializer(data=tokenData)
+        tokenSerializer.is_valid(raise_exception=True)
+        
+        return Response(tokenSerializer.validated_data, status=status.HTTP_201_CREATED)
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UserSerializer
+
+
+class VerifyTokenView(TokenVerifyView):
+    def post(self, request, *args, **kwargs):
+        serializer = TokenVerifySerializer(data=request.data)
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        try:
+            serializer.is_valid(raise_exception=True)
+            token_data = tokenBackend.decode(request.data['token'],verify=False)
+            serializer.validated_data['UserId'] = token_data['usuario_cedula']
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
