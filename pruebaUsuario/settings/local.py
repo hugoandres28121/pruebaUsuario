58db9ff56d7030778faf201cdc721068ff5d0f5e
HugoMarrugo <hugoandresmarrugopolo@gmail.com>
2021-11-17 14:27:46 -0500

    Microservicio Users

--- pruebaUsuario/settings/local.py
+++ pruebaUsuario/settings/local.py
@@ -5,17 +5,18 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'dauobm2777lbss',
+    'USER': 'ygzbxaqexwjfsw',
+    'PASSWORD': 'c6c1587c40f318b5abb4eeeb3eccb39541429ceae6290749c01e146a761f2689',
+    'HOST': 'ec2-3-231-69-204.compute-1.amazonaws.com',
+    'PORT': '5432',
     }
 }
-
 STATIC_URL = '/static/'
 
